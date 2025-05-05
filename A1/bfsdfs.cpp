464a6c0196625038d2f0056738f9ea37e2c1d513
pma2012 <anapatprabhu@gmail.com>
2025-05-05 20:30:05 +0530

    added

---
+++ A1/bfsdfs.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs(vector<int> adj[], int start, int n)
+{
+    queue<int> q;
+    vector<int> vis(n + 1, 0);
+    q.push(start);
+    vis[start] = 1;
+    while (!q.empty())
+
+    {
+        int front = q.front();
+        q.pop();
+        cout << front << " ";
+        for (int i = 0; i < adj[front].size(); i++)
+        {
+            if (!vis[adj[front][i]])
+            {
+                q.push(adj[front][i]);
+                vis[adj[front][i]] = 1;
+            }
+        }
+    }
+}
+void dfs(vector<int> adj[], int vis[], int start)
+{
+    cout << start << " ";
+    vis[start] = 1;
+    for (int i = 0; i < adj[start].size(); i++)
+    {
+        if (!vis[adj[start][i]])
+        {
+            dfs(adj, vis, adj[start][i]);
+        }
+    }
+}
+void dfs2(vector<int> adj[], int start, int n) // iterative
+{
+    stack<int> st;
+    vector<int> vis(n + 1, 0);
+    st.push(start);
+    vis[start] = 1;
+    while (!st.empty())
+
+    {
+        int top = st.top();
+        st.pop();
+        cout << top << " ";
+        for (int i = 0; i < adj[top].size(); i++)
+        {
+            if (!vis[adj[top][i]])
+            {
+                st.push(adj[top][i]);
+                vis[adj[top][i]] = 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int n, m;
+    cout << "enter num of nodes" << endl;
+    cin >> n;
+    cout << "enter num of edges" << endl;
+    cin >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "->";
+        for (int j = 0; j < adj[i].size(); j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+    bfs(adj, 1, n);
+    int vis[n + 1] = {0};
+    cout << endl;
+    dfs(adj, vis, 1);
+    cout << endl;
+    dfs2(adj, 1, n);
+    return 0;
+}
+// 2 1 1 3 3 4 2 4 2 5 4 5
